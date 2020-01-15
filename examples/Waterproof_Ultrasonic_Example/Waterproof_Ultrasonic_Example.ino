efa93f3b3024597e8bc40ffd4830f44f6a766487
devilthanh <33759203+devilthanh@users.noreply.github.com>
2020-01-15 17:52:58 +0700

    Add files via upload

---
+++ examples/Waterproof_Ultrasonic_Example/Waterproof_Ultrasonic_Example.ino
@@ -0,0 +1,14 @@
+#include <Waterproof_Ultrasonic.h>
+
+
+Waterproof_Ultrasonic cambien(11,12,3);
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  uint16_t value = cambien.read();
+  if(value) Serial.println("Testing with MODE " + String(cambien.getMode()) + ", R27 = " + cambien.getR27() + ", value = " + String(value) + " cm");
+  delay(500);
+}
